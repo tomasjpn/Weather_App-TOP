01e7a754352aece973b1ce1ee816ebfe7ec20154
tomasjpn <tomasphammc@gmail.com>
2024-06-20 21:22:35 +0200

    api.js: Code-Refactoring mit Async/Await

--- generate-cert.js
+++ generate-cert.js
@@ -8,3 +8,9 @@ fs.writeFileSync('cert.pem', pems.cert);
 fs.writeFileSync('key.pem', pems.private);
 
 console.log('Certificates generated successfully.');
+
+/**
+ * Ein lokaler HTTPS Server wird mit Node.js erstellt, um eigenen SSL Zertifikat zu verwenden.
+ * 
+ * http-server -S -C cert.pem -K key.pem -> Command um das Projekt über den lokalen Server laufen zu lassen, da die API Http Server aus Sicherheitsgründen nicht zulässt
+ */
--- src/api.js
+++ src/api.js
@@ -1,55 +1,52 @@
 // API-Aufruf für die Wetterabfrage und Weiterverarbeitung
-
 import { showHourlyForecast } from "./hourlyForecast";
 import { showWeatherData, showWeatherData2 } from "./weatherDisplay";
 
-//API Key 
+// API Key
 const apiKey = "1ab9663b28c7c70557cf8779d5ca5c79";
 
 // Funktion für den Aufruf der Wetterdaten aus den Koordinaten
-export function getWeatherDataByCoords(lat, lon) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`)
-        .then(res => res.json())
-        .then(data => {
-            const cityName = data.name;
-            const countryName = data.sys.country;
-            showWeatherData(data, cityName, countryName); // Daten aus der API werden an die showWeatherData weitergegeben
-            getWeatherData(lat, lon); // Abrufen weiterer Wetterdaten wie die detaillierte Wettervorhersage
-            console.log(data);
-        })
-        .catch(err => {
-            console.error('Error fetching weather data:', err);
-        });
+export async function getWeatherDataByCoords(lat, lon) { //Funktion muss mit Async deklariert werden, um await zu verwenden
+    try { // Der Code-Block wird ausgeführt, falls nicht möglich -> catch err
+        // await wie Promise -> Der gesamte Code wird erst weiter geführt, wenn die fetch Anfrage fertig ist.
+        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`);
+        const data = await res.json();
+        const cityName = data.name;
+        const countryName = data.sys.country;
+        showWeatherData(data, cityName, countryName); // Daten aus der API werden an die showWeatherData weitergegeben
+        await getWeatherData(lat, lon); // Abrufen weiterer Wetterdaten wie die detaillierte Wettervorhersage
+        console.log(data);
+    } catch (err) {
+        console.error('Error fetching weather data:', err);
+    }
 }
 
 // Funktion für den Aufruf der Wetterdaten aus der Suchleiste für Namen
-export function getWeatherDataByCity(city) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
-        .then(res => res.json())
-        .then(data => {
-            const latitude = data.coord.lat;
-            const longitude = data.coord.lon;
-            const cityName = data.name;
-            const countryName = data.sys.country;
-            showWeatherData(data, cityName, countryName);
-            getWeatherData(latitude, longitude);
-            console.log(data);
-        })
-        .catch(err => {
-            console.error('Error fetching weather data:', err);
-        });
+export async function getWeatherDataByCity(city) {
+    try {
+        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
+        const data = await res.json();
+        const latitude = data.coord.lat;
+        const longitude = data.coord.lon;
+        const cityName = data.name;
+        const countryName = data.sys.country;
+        showWeatherData(data, cityName, countryName);
+        await getWeatherData(latitude, longitude);
+        console.log(data);
+    } catch (err) {
+        console.error('Error fetching weather data:', err);
+    }
 }
 
 // Wetterdaten für die Wettervorhersage
-function getWeatherData(lat, lon) {
-    fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`)
-        .then(res => res.json())
-        .then(data => {
-            showWeatherData2(data);
-            showHourlyForecast(data); // Daten aus der API an showHourlyForecast weitergeben
-            console.log(data);
-        })
-        .catch(err => {
-            console.error('Error fetching weather data:', err);
-        });
+async function getWeatherData(lat, lon) {
+    try {
+        const res = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`);
+        const data = await res.json();
+        showWeatherData2(data);
+        showHourlyForecast(data); // Daten aus der API an showHourlyForecast weitergeben
+        console.log(data);
+    } catch (err) {
+        console.error('Error fetching weather data:', err);
+    }
 }
